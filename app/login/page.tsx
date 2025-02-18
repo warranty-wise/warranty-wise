020befacfc4c471c842f139a0a031e2a8bc0eede
Jiawen Chen <jiawenchen213@gmail.com>
2025-02-17 16:15:22 -1000

    added simple styling changes to log in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,13 +2,16 @@ import { login, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className='container'>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button className ="login" formAction={login}>Log in</button>
+        <div className = 'divider' />
+        <button className ="signup" formAction={signup}>Sign up</button>
+      </form>
+    </div>
   )
 }
