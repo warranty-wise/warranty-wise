a8e7d51fffe9fb04f5d60bad08ee84087120d9b4
Jiawen Chen <jiawenchen213@gmail.com>
2025-04-12 15:58:09 -1000

    implemented add function for calendar events issue#28

---
+++ app/calendar/actions.ts
@@ -0,0 +1,26 @@
+'use client'
+
+import { createClient } from "@/utils/supabase/client"
+
+const supabase = createClient()
+
+export async function addCalendarEvent(warranty_id: string, user_id: string) {
+    try {
+        // insert new calendar event
+        const { error } = await supabase
+            .from('events')
+            .insert([
+                {
+                    user_id: user_id,
+                    warranty_id: warranty_id,
+                }
+            ])
+
+        if (error) {
+            throw error
+        }
+    } catch (error) {
+        console.error('Error creating calendar event:', error)
+        throw error
+    }
+}
