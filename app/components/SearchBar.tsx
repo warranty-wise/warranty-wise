b442d6b8df8b4b5377c23d8930cb57e5d3d0e982
Jiawen Chen <jiawenchen213@gmail.com>
2025-04-25 13:37:38 -1000

    implemented searchbar to search by product names and product manufacturers

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
-const AutocompleteSearchBar  = ({query, setQuery}) => {
+const SearchBar  = ({ onSearch }) => {
+    const [query, setQuery] = useState("")
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = e.target.value
+        setQuery(value)
+        onSearch(value)
+    }
+
     return (
-        <div
+        <form
             style={{
                 padding: "8px",
                 borderRadius: "8px",
@@ -17,12 +26,11 @@ const AutocompleteSearchBar  = ({query, setQuery}) => {
                 boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)", // Add this line for box shadow
             }}
         >
-
             <IoIosSearch className="size-7 invert"/>
             <input
                 type="text"
                 value={query}
-                onChange={(e) => setQuery(e.target.value)}
+                onChange={(e) => handleSearch(e)}
                 style={{
                     width: "100%",
                     border: 'none',
@@ -31,7 +39,7 @@ const AutocompleteSearchBar  = ({query, setQuery}) => {
                     zIndex: "1",
                 }}
             />
-        </div>
+        </form>
     );
 }
-export default AutocompleteSearchBar
+export default SearchBar
